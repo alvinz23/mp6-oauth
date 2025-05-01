80d3905da55aeb3ab086d20b9ea848bf0beac9ea
alvin <alvinzhu65@gmail.com>
2025-05-01 04:13:00 -0400

    test refresh

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { SignInButton } from "../app/components/SignInButton";
 import UserGreeting from "../app/components/UserGreeting";
+import { redirect } from "next/navigation"; 
 
 export default async function HomePage(props: { searchParams: Promise<{ code?: string | null; error?: string | null }> }) {
   const { code } = await props.searchParams;
 
   if (!code) {
-    return <SignInButton />;
+    return (
+      redirect("https://mp6-oauth-pl2e.vercel.app/"), 
+    <SignInButton />
+    );
   }
 
   return (
