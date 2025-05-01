ae8407f7f41b663b0eaa6410f2e3eef5bfbc0d17
alvin <alvinzhu65@gmail.com>
2025-04-30 23:48:20 -0400

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 // app/page.tsx
-import { SignInButton } from "../app/components/SignInButton"
+import { SignInButton } from "../app/components/SignInButton";
 import UserGreeting from "../app/components/UserGreeting";
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: { code?: string; error?: string };
-}) {
-  const { code } = searchParams;
+interface PageProps {
+  searchParams: {
+    code?: string | null;
+    error?: string | null;
+  };
+}
+
+export default async function HomePage({ searchParams }: PageProps) {
+  const { code, error } = searchParams;
 
   if (!code) {
     return <SignInButton />;
