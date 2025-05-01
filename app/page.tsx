67a95ea76cff61c5f85a6f4e6412a16777f46a73
alvin <alvinzhu65@gmail.com>
2025-04-30 23:54:11 -0400

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 import { SignInButton } from "../app/components/SignInButton";
 import UserGreeting from "../app/components/UserGreeting";
 
-interface PageProps {
-  searchParams: {
-    code?: string | null;
-    error?: string | null;
-  };
-}
-
-export default async function HomePage({ searchParams }: PageProps) {
-  const { code } = searchParams; 
+export default async function HomePage(props: { searchParams: Promise<{ code?: string | null; error?: string | null }> }) {
+  const { code } = await props.searchParams;
 
   if (!code) {
     return <SignInButton />;
